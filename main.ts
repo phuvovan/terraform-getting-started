4cb153309085533a66a255fa803ee8ab191d4f47
phuvovan <100821176+phuvovan@users.noreply.github.com>
2023-02-25 17:51:34 +0700

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,19 @@
+provider "google" {
+project = "phuvo-accenture"
+region = "asia-southeast1"
+}
+
+resource "google_compute_instance" "myvm" {
+name = "myvm-dev"
+zone = "asia-southeast1-c"
+boot_disk {
+initialize_params {
+  image = "debian-cloud/debian-9"
+  size = "10"
+}
+}
+machine_type = "f1-micro" 
+network_interface {
+network = "default"
+}
+}
